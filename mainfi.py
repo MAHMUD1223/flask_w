38bf9f6d18a9cb32296681500b445e289fa2b253
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-02-12 13:11:27 +0000

    2nd db add uncom

--- mainfi.py
+++ mainfi.py
@@ -13,17 +13,17 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=20), unique=True)
     pasd = db.Column(db.String(length=4))
-#    owend= db.Column(db.Integer(), db.ForeignKey("log.id"))
-#    def __repr__(self):
-#        return f'admin {self.name}'
-#
-#class Log(db.Model):
-#    id = db.Column(db.Integer(), primary_key=True)
-#    username = db.Column(db.String(length=10), nullable=False, unique=True )
-#    email_address = db.Column(db.String(length=30), nullable=False, unique=True)
-#    pass_hash = db.Column(db.String(length=60), nullable=False)
-#    budget = db.Column(db.INTEGER(), nullable=False, default=1000)
-#    result = db.relationship('User', backref='owend_user', lazy=True)
+    owend= db.Column(db.Integer(), db.ForeignKey("log.id"))
+    def __repr__(self):
+        return f'admin {self.name}'
+
+class Log(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=10), nullable=False, unique=True )
+    email_address = db.Column(db.String(length=30), nullable=False, unique=True)
+    pass_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.INTEGER(), nullable=False, default=1000)
+    result = db.relationship('User', backref='owend_user', lazy=True)
 
 
 
@@ -40,7 +40,8 @@ def index():
 def user(alpha):
     if alpha=='12345':
         user = User.query.all()
-        return render_template('adam.html', user=user)
+        log = Log.query.all()
+        return render_template('adam.html', user=user, log=log)
     else:
         return f"ah ah a you didn't say the magic word insted said {alpha}"
 
