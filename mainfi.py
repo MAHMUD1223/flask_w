4d47d5923afb9505ba31ddfabddf744a4f501bc4
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-02-12 11:47:04 +0000

    addin database sql lite

--- mainfi.py
+++ mainfi.py
@@ -1,30 +1,32 @@
 #created on 9/2/2023 at 6p.m
 from flask import Flask, render_template
 app = Flask(__name__)
-'''
+
 #sqllight db
 from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 db = SQLAlchemy(app)
 
 
-class user(db.Model):
-    name = db.Column(db.String(length=20), nullable=False, unique=True)
-'''
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=20), unique=True)
+    pasd = db.Column(db.String(length=4))
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/ad')
-def adam():
-    return render_template('adam.html')
-
 @app.route('/admin/<alpha>')
 def user(alpha):
     if alpha=='12345':
-        return 'welcome'
+        user = User.query.all()
+        return render_template('adam.html', user=user)
     else:
-        return f"hell{alpha}"
+        return f"ah ah a you didn't say the magic word insted said {alpha}"
 
 #@app.route('/about/<username>')
 #def about(username):
